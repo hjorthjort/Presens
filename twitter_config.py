71924100275e1794f0cb7e96c266b1fbe4c391de
Tim Kerschbaumer <Tim@Tims-MacBook-Pro.local>
2015-12-06 00:57:48 +0000

    added classes for quering twitter api

---
+++ twitter_config.py
@@ -0,0 +1,6 @@
+# Twitter API credentials
+
+consumer_key = "VFv3Y2rTA2PCQq3B3XqZ62LBi"
+consumer_secret = "X5htOjBF26AkQusaRR4qfSq3lgh2qyoZjP79tsC30Fc2UZoUWd"
+access_key = "406387182-8zPFOmomgoOfH43gule2cJTRxx9suA4sson9ozNM"
+access_secret = "XY0XrRhidIFEcbCCVUNtES9X9ft6LF6DbWdC7BIJ4J8KA"
