5538fe3d15224ce61a5ef5d75c8190d864262f56
Tim Kerschbaumer <Tim@Tims-MacBook-Pro.local>
2015-12-06 04:11:21 +0000

    Added some recommendation filtering based on twitter trends

--- alchemy.py
+++ alchemy.py
@@ -1,11 +1,8 @@
 import alchemyapi
 
-
 class alchemy:
-
     def __init__(self):
         self.api_connection = alchemyapi.AlchemyAPI()
-
 
     def find_keywords(self, text):
         return self.api_connection.keywords("text", text, {'showSourceText': 0})
---
+++ trend_filtering.py
@@ -0,0 +1,34 @@
+from __future__ import division
+import argparse
+import numpy as np
+from alchemy import alchemy
+from twitter_trends import twitter
+
+class trend_filtering:
+    def __init__(self):
+        self.al_object = alchemy()
+        self.tw_object = twitter()
+
+    def prioritise_on_trends(self, metadata_list, trends_list):
+        priority_list = []
+        trending_concepts = self.get_trending_concepts()
+        for i in range(0, metadata_list[:,0].size):
+            metadata_concepts = self.get_metadata_concepts(metadata[i,:])
+            for k in range(0, len(metadata_concepts))
+                for j in range(0, len(trending_concepts)):
+                    if metadata_concepts[k] == trending_concepts[j]:
+                        priority_list.append(metadata_list[i,:])
+        return priority_list
+
+    def get_metadata_concepts(self, metadata):
+        concepts = []
+        concepts.append(self.al_object.find_keywords(". ".joint(metadata)))
+        return concepts
+
+    def get_trending_concepts(self):
+        concepts = []
+        for trend in self.tw_object.get_trends():
+            tweets = self.tw_object.get_tweets(trend)
+            concept = self.al_object.find_keywords(". ".join(tweets))
+            concepts.append(concept)
+        return concepts
