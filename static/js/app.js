ea08fc700f8a41ed602b3ce26274e558ebfec5f6
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-12-06 02:11:15 +0100

    fetching list from local api

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,10 @@ angular.module('recApp', [])
 
 .controller('RecommendCtrl', ['$scope', '$http', function($scope, $http) {
 		$scope.list = "Some list";
-
+		$http.get('http://presens.eu-gb.mybluemix.net/api/lists').
+			success(function(data) {
+				$scope.list = data.lists[0].title;
+			})
 
 	}]);
 
