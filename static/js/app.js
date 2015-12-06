94dd8f9be2be4d4199858be7202d452de80f152c
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-12-06 01:58:14 +0100

    Some angular

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,12 @@
 'use strict';
 
-angular.module('App', [
-	]);
+angular.module('recApp', [])
+
+.controller('RecommendCtrl', ['$scope', '$http', function($scope, $http) {
+		$scope.list = "Some list";
+
+
+	}]);
+
+
+;
