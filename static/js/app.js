c39900179909ebbac9c81dac27e9d6d89fa74065
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-12-06 02:22:28 +0100

    repeat of reccomendations

--- static/js/app.js
+++ static/js/app.js
@@ -3,10 +3,11 @@
 angular.module('recApp', [])
 
 .controller('RecommendCtrl', ['$scope', '$http', function($scope, $http) {
-		$scope.list = "Some list";
+
 		$http.get('http://presens.eu-gb.mybluemix.net/api/lists').
 			success(function(data) {
 				$scope.list = data.lists[0].title;
+				$scope.items = data.lists[0].items;
 			})
 
 	}]);
